032109be13b97732ba1cc556ea2d0725e733974a
Sujitha1306 <sujitha1366@gmail.com>
2025-07-21 15:48:49 +0530

    new ex

--- ex7/script.js
+++ ex7/script.js
@@ -1,63 +1,36 @@
-let display = document.getElementById('display');
-let memory = 0;
+let display = document.getElementById("display");
 
 function append(value) {
-  if (display.value === "0") display.value = "";
   display.value += value;
 }
 
-function clearAll() {
-  display.value = "0";
-}
-
-function clearEntry() {
-  display.value = "0";
-}
-
 function backspace() {
-  display.value = display.value.slice(0, -1) || "0";
+  display.value = display.value.slice(0, -1);
 }
 
 function calculate() {
   try {
-    display.value = eval(display.value.replace('÷', '/').replace('×', '*'));
-  } catch (e) {
+    display.value = eval(display.value.replace(/mod/g, '%'));
+  } catch {
+    display.value = "Error";
+  }
+}
+function clearDisplay() {
+  display.value = "";
+}
+
+function sqrt() {
+  try {
+    display.value = Math.sqrt(eval(display.value));
+  } catch {
     display.value = "Error";
   }
 }
 
-function sqrt() {
-  display.value = Math.sqrt(eval(display.value));
-}
-
 function square() {
-  display.value = Math.pow(eval(display.value), 2);
-}
-
-function toggleSign() {
-  if (display.value.charAt(0) === '-') {
-    display.value = display.value.slice(1);
-  } else {
-    display.value = '-' + display.value;
+  try {
+    display.value = Math.pow(eval(display.value), 2);
+  } catch {
+    display.value = "Error";
   }
-}
-
-function memoryClear() {
-  memory = 0;
-}
-
-function memoryRecall() {
-  display.value = memory;
-}
-
-function memoryAdd() {
-  memory += eval(display.value);
-}
-
-function memorySubtract() {
-  memory -= eval(display.value);
-}
-
-function memoryStore() {
-  memory = eval(display.value);
 }
